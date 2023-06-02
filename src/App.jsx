4ce40b69ebe504ceb64c0f43019c7fa607e05bc3
juantoranzos <toranzos628@gmail.com>
2023-06-02 17:40:29 -0300

    agregamos el componente frase

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './assets/logosimpson.png'
+import { Container, Button } from 'react-bootstrap';
+import Frase from './components/Frase';
+
 
 function App() {
- 
-
   return (
     <>
-   
+<Container className='text-center my-5'>
+      <img src={logo} alt="logo de los simpson"  className='w-50'/>
+      <Frase/>
+      <Button variant='warning'> Obtener frase</Button>
+  
+</Container>
+
     </>
   )
 }
 
 export default App
+
