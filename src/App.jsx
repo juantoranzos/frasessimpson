d9a03d8070c0686bbdd2611e37a26217d055b2f6
juantoranzos <toranzos628@gmail.com>
2023-06-02 21:53:03 -0300

    agregamos un spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,50 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './assets/logosimpson.png'
-import { Container, Button } from 'react-bootstrap';
+import { Container, Button, Spinner } from 'react-bootstrap';
 import Frase from './components/Frase';
 import { useEffect, useState } from 'react';
 
 
 function App() {
   const [personaje, setPersonaje] = useState({});
-  useEffect (()=>{
-   consultarAPI()
-  },[])
-  const consultarAPI = async ()=>{
+  const [mostrarSpinner, setMostrarSpinner] = useState(true);
+  useEffect(() => {
+    consultarAPI()
+  }, [])
+  const consultarAPI = async () => {
     //realizar una peticion get
     //https://thesimpsonsquoteapi.glitch.me/quotes
-    try{
+    try {
+      setMostrarSpinner(true);
       const respuesta = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
       const dato = await respuesta.json()
       console.log(respuesta)
       console.log(dato[0])
       setPersonaje(dato[0])
-    }catch(error){
+      //ocultar el spinner 
+      setTimeout ( () =>{
+        
+        setMostrarSpinner(false)
+      }, 2500)
+    } catch (error) {
       console.log(error);
     }
-    
+  };
+  const mostrarComponente = (mostrarSpinner) ? (<div className='my-5'>
 
-  }
+    <Spinner></Spinner>
+  </div>) : <Frase personaje={personaje} />
   return (
     <>
-<Container className='text-center my-5'>
-      <img src={logo} alt="logo de los simpson"  className='w-50'/>
-      <Frase personaje ={personaje}/>
-      <Button variant='warning' onClick={consultarAPI}> Obtener frase</Button>
-  
-</Container>
+      <Container className='text-center my-5'>
+        <img src={logo} alt="logo de los simpson" className='w-50' />
+       {
+        mostrarComponente
+       }
+        <Button variant='warning' onClick={consultarAPI}> Obtener frase</Button>
+
+      </Container>
 
     </>
   )
